8e99adea05506914c70cf1096dfee41db3f82165
BeauTheBeau <beauthebeau@proton.me>
2023-10-13 20:19:38 +0100

    Remade entire website
    Adopting a new bento grid layout, inspired by Alistair Smith https://alistair.sh/

--- js/main.js
+++ js/main.js
@@ -1,23 +1,133 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 30,
+let discordInfo;
+let githubInfo;
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+/**
+ * @name getDiscordInfo
+ * @description Fetches a user's Discord information from the API
+ * @param {string} userId The user's ID
+ * @returns {Promise<void>}
+ */
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+async function getDiscordInfo(userId) {
+    const apiUrl = `https://api.lanyard.rest/v1/users/`;
+    const response = await fetch(apiUrl + userId);
+    const data = await response.json();
+    return data.data;
+}
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+/**
+ * @name getGithubInfo
+ * @description Fetches a user's GitHub information from the API
+ * @param {string} username The user's username
+ * @returns {Promise<void>}
+ */
+
+async function getGithubInfo(username) {
+
+    const apiUrl = `https://api.github.com/users/`;
+    const response = await fetch(apiUrl + username);
+    return await response.json();
+
+}
+
+/**
+ * @name setDiscordInfo
+ * @description Sets the user's Discord information on the page
+ * @param {object} data The user's Discord information
+ * @param {HTMLElement} element The element to set the information on
+ * @returns {void}
+ */
+
+function setDiscordInfo(data, element) {
+
+    const normalData = {
+        username: data.discord_user.username,
+        avatar: data.discord_user.avatar,
+        id: data.discord_user.id,
+        status: data.discord_status
+    };
+
+
+    const h2 = element.querySelector("h2");
+    h2.innerHTML = normalData.username;
+
+    const a = element.querySelector("a");
+    a.href = `https://discord.com/users/${normalData.id}`;
+
+    element.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("https://cdn.discordapp.com/avatars/${normalData.id}/${normalData.avatar}.webp?size=512")`;
+}
+
+/**
+ * @name setGithubInfo
+ * @description Sets the user's GitHub information on the page
+ * @param {object} data The user's GitHub information
+ * @param {HTMLElement} element The element to set the information on
+ * @returns {void}
+ */
+
+function setGithubInfo(data, element) {
+
+    const normalData = {
+        username: data.login,
+        avatar: data.avatar_url
+    }
+
+    const h2 = element.querySelector("h2");
+    h2.innerHTML = normalData.username;
+
+    const a = element.querySelector("a");
+    a.href = `https://github.com/${normalData.username}`;
+
+    element.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("${normalData.avatar}")`;
+}
+
+getDiscordInfo("729567972070391848").then(data => {
+    discordInfo = data;
+    setDiscordInfo(discordInfo, document.getElementById("discord"));
 });
+
+getGithubInfo("BeauTheBeau").then(data => {
+    githubInfo = data;
+    setGithubInfo(githubInfo, document.getElementById("github"));
+});
+
+
+let contentArray = [ "profile", "twitter", "about", "github", "discord", "time", "location", "skills", "projects", "contact" ];
+
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card, index) => {
+    card.style.animationDelay = `${index * 100}ms`;
+    card.style.gridArea = contentArray[index];
+    card.classList.add(contentArray[index])
+    card.id = contentArray[index];
+    if (card.innerHTML === "") card.innerHTML = `<h2>` + contentArray[index][0].toUpperCase() + contentArray[index].slice(1) + `</h2>`;
+});
+
+
+
+
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
+    randomString = length => Array.from(Array(length)).map(randomChar).join("");
+
+const letters = document.querySelectorAll(".letter");
+
+const handleOnMove = e => {
+    const tgtCard = e.currentTarget;
+    const letter = tgtCard.querySelector(".card-letters");
+
+    let rect = letter.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    letter.style.setProperty("--x", `${x}px`);
+    letter.style.setProperty("--y", `${y}px`);
+
+    letter.innerText = randomString(1500);
+    console.log(letter.innerText);
+}
+
+cards.forEach(card => { card.addEventListener("mousemove", handleOnMove); } );
